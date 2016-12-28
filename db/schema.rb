9c240cef1596f6dcdd734990a450875aac7fc40d
lankireddy <jagan4189@gmail.com>
2016-12-28 20:17:01 +0530

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223074203) do
+ActiveRecord::Schema.define(version: 20161228142613) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
