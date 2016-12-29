8aab7cd85ddb53cfba5a3df175420bbdbcd18537
lankireddy <jagan4189@gmail.com>
2016-12-29 15:55:21 +0530

    name added to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229070005) do
+ActiveRecord::Schema.define(version: 20161229102215) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20161229070005) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
